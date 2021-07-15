efb54a2b01ffcdb7f867399f09a6c8e4d2c28b7e
Nicolas Marri <nicomarri22@gmail.com>
2021-07-15 20:50:18 -0300

    creando jwt

--- servidor/index.js
+++ servidor/index.js
@@ -7,9 +7,15 @@ const app = express()
 //CONECTAR A LA BASE DE DATOS
 conectarDB();
 
+//HABILITAR EXPRESS.JSON
+app.use(express.json({extended: true}));
+
 //PUERTO DE LA APP
 const PORT = process.env.PORT || 4000;
 
+//IMPORTAR RUTAS
+app.use('/api/usuarios', require ('./routes/usuarios'));
+app.use('/api/auth', require ('./routes/auth'));
 
 //ARRANCAR LA APP
 app.listen(PORT, () => {
