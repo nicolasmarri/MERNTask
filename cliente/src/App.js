16f1afcdab2bd62265854d1d0a8a4a10f201595a
Nicolas Marri <nicomarri22@gmail.com>
2021-07-20 16:35:49 -0300

    adding alert state

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './components/auth/Login'
 import NuevaCuenta from './components/auth/NuevaCuenta'
 import Proyectos from './components/proyectos/Proyectos'
+import AlertaState from './context/alertas/alertaState'
 
 import ProyectoState from './context/proyectos/proyectoState'
 import TareaState from './context/tareas/tareaState'
 
+
 function App() {
   return (
     <ProyectoState>
-      <TareaState>  
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/nueva-cuenta' component={NuevaCuenta} />
-            <Route exact path='/proyectos' component={Proyectos} /> 
-          </Switch>
-        </Router>
+      <TareaState>
+        <AlertaState>
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route exact path='/nueva-cuenta' component={NuevaCuenta} />
+              <Route exact path='/proyectos' component={Proyectos} />
+            </Switch>
+          </Router>
+        </AlertaState>
       </TareaState>
     </ProyectoState>
   );
